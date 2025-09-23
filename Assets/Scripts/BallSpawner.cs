5a50efccc1c0d68e3731c47f6705b88020efdfd2
Suppeerr <107443707+Suppeerr@users.noreply.github.com>
2025-09-23 18:57:17 -0400

    Made homing cannon projectile and projectile launchers

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    public GameObject cannon_projectile;
+    public float spawnPerSecond = 10f;
+    private float spawnRate;
+    private float timer = 0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawnRate = 1f / spawnPerSecond;
+        spawnBall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            spawnBall();
+            timer = 0;
+        }
+    }
+
+    void spawnBall()
+    {
+        Instantiate(cannon_projectile, transform.position, transform.rotation);
+
+    }
+}
