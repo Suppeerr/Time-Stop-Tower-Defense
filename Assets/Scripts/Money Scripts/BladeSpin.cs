893f8c6f7bc3b4c805e94f57df42e81498b03034
Suppeerr <107443707+Suppeerr@users.noreply.github.com>
2025-10-26 20:08:59 -0400

    Colored drone and made propeller blades rotate

---
+++ Assets/Scripts/Money Scripts/BladeSpin.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class BladeSpin : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 1000f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime, Space.Self);
+    }
+}
