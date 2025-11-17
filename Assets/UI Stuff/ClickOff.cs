35a6aa8faec1ede0222c558b0f6d5d720bafe757
justsomeboy26 <zhangjimmy31@gmail.com>
2025-11-17 09:15:29 -0500

    Insane scuff
    Trying to figure out whats going wrong. commiting this for now

--- Assets/UI Stuff/ClickOff.cs
+++ Assets/UI Stuff/ClickOff.cs
@@ -4,20 +4,33 @@ using UnityEngine.EventSystems;
 public class ClickOff : MonoBehaviour
 {
 
-    void Update(){
-        if (Input.GetMouseButtonDown(0) && this.gameObject.activeSelf == true && !IsPointerOverUIElement()){
-            Debug.Log(gameObject.activeSelf);
-            Debug.Log("REMOVING UI");
-            this.gameObject.SetActive(false);
+    void Update()
+ {
+        //Check for mouse click 
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out raycastHit, 100f))
+            {
+                if (raycastHit.transform != null)
+                {
+                   //Click method
+                    Debug.Log("CLICKING");
+                    CurrentClickedGameObject(raycastHit.transform.gameObject);
+                }
+            }
         }
-    }
+ }
 
-    // Helper function to check if the pointer is over a UI element
-    private bool IsPointerOverUIElement()
+public void CurrentClickedGameObject(GameObject gameObject)
+{
+    if(gameObject.tag != "Workshop UI")
     {
-        // Check if the current selected object is a UI element
-        return EventSystem.current.currentSelectedGameObject != null;
+        Debug.Log(gameObject.tag);
+        this.gameObject.SetActive(false);
     }
+}
 
 
 }
